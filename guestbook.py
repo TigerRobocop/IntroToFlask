66c61625901eed3a16d7c58e35206e55c902e34a
Lívia Souza <lmrs@cesar.org.br>
2018-06-15 15:36:18 -0300

    Templates

--- guestbook.py
+++ guestbook.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,8 +7,9 @@ def index():
     return '<h1>Hello There</h1>'
 
 @app.route('/home', methods=['GET', 'POST'])
-def home(place):
-    return '<h1>Welcome to home page</h1>'
+def home():
+    links = [{ "url" : "https://www.google.com", "text": "Google" }, { "url" : "https://www.facebook.com", "text": "Facebook" }, { "url" : "https://www.udemy.com", "text": "Udemy" }]
+    return render_template('template.html', my_var="Liv", links=links) 
 
 # @app.route('/home/<place>')
 # def home(place):
