2bcfdb7b0bbeae48f3668dc2e1fe00533362d38e
Lívia Souza <lmrs@cesar.org.br>
2018-06-15 18:45:34 -0300

    removed commented code

--- guestbook.py
+++ guestbook.py
@@ -38,9 +38,5 @@ def home():
     links = [{ "url" : "https://www.google.com", "text": "Google" }, { "url" : "https://www.facebook.com", "text": "Facebook" }, { "url" : "https://www.udemy.com", "text": "Udemy" }]
     return render_template('template.html', my_var="Liv", links=links) 
 
-# @app.route('/home/<place>')
-# def home(place):
-#     return '<h1>Welcome to ' + place + ' page</h1>'
-
 if __name__ == '__main__':
     app.run(debug=True)
