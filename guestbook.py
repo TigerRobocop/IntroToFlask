e8bdb1c1bef121ca19b8f5c3bc4452d414cdabb1
Lívia Souza <lmrs@cesar.org.br>
2018-06-15 18:03:27 -0300

    SQl Alchemy installed, DB setup and create + read methods

--- guestbook.py
+++ guestbook.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql10243121:jEbSnYMlax@sql10.freemysqlhosting.net/sql10243121'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(20))
+    content = db.Column(db.String(1000))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    results = Comments.query.all()
+
+    return render_template('index.html', results = results)
 
 @app.route('/sign')
 def sign():
@@ -12,9 +24,14 @@ def sign():
 
 @app.route('/process', methods=['POST'])
 def process():
-    name = request.form['name']
-    comment = request.form['comment']
-    return render_template('index.html', author=name, content=comment)
+    author = request.form['author']
+    content = request.form['content']
+
+    signature = Comments(author = author, content = content)
+    db.session.add(signature)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
