6ff2c310ec3cde88f3c28eead8cec87b634ec119
Lívia Souza <lmrs@cesar.org.br>
2018-06-15 16:14:06 -0300

    index and sign pages

--- guestbook.py
+++ guestbook.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello There</h1>'
+    return render_template('index.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    return render_template('index.html', author=name, content=comment)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
